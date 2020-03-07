afc6462742e853ee6af63ed80498d97757afe374
weijin <1455236662@qq.com>
2020-03-07 11:11:00 +0800

    update: change the way to get hostname

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 axios.defaults.baseURL = process.env.NODE_ENV === 'production'
-  ? 'http://localhost:9090/api'
+  ? 'http://' + location.hostname +':80/api'
   : 'http://localhost:8080/api';
 
 export const getPaper = (id) =>{
